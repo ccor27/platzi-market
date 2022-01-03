193589b31816e69a80c42b5e2576c768f5207674
cristian <cris300oso@gmail.com>
2022-01-03 20:54:32 +0100

    documentacion con swagger

--- src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
@@ -1,5 +1,74 @@
 package com.platzi.market.persistence.entity;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+    @EmbeddedId
+    private ComprasProductoPK id;
+
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
+
+    @ManyToOne
+    @MapsId("idCompra")
+    @JoinColumn(name = "id_compra", insertable = false, updatable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", insertable = false, updatable = false)
+    private Producto producto;
+
+    public ComprasProductoPK getId() {
+        return id;
+    }
+
+    public void setId(ComprasProductoPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public Compra getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Compra compra) {
+        this.compra = compra;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
+/*
 import javax.persistence.*;
 
 @Entity
@@ -69,4 +138,4 @@ public class ComprasProducto {
     public void setProducto(Producto producto) {
         this.producto = producto;
     }
-}
+}*/
--- src/main/java/com/platzi/market/web/controller/PurchaseController.java
+++ src/main/java/com/platzi/market/web/controller/PurchaseController.java
@@ -1,4 +1,5 @@
-package com.platzi.market.persistence.web.controller;
+package com.platzi.market.web.controller;
+
 
 import com.platzi.market.domain.Purchase;
 import com.platzi.market.domain.service.PurchaseService;
@@ -31,5 +32,29 @@ public class PurchaseController {
     public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
         return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
     }
-
 }
+/*
+@RestController
+@RequestMapping("/push")
+public class PurchaseController {
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Purchase>> getAll() {
+        return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/client/{idClient}")
+    public ResponseEntity<List<Purchase>> getByClient(@PathVariable("idClient") String clientId) {
+        return purchaseService.getByClient(clientId).map(
+                purchases -> new ResponseEntity<>(purchases, HttpStatus.OK)
+        ).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
+        return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
+    }
+
+}*/

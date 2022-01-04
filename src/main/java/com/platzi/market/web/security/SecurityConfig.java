3ff8f090ae769f36cd2205f86d3155d26eb512cc
cristian <cris300oso@gmail.com>
2022-01-04 14:04:57 +0100

    genera jwt y gestion de autenticacion

--- src/main/java/com/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.platzi.market.web.security;
 
 import com.platzi.market.domain.service.PlatziUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -15,5 +18,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(platziUserDetailsService);
+    }
+
+    //vamos a decirle a la clase que no se necesita seguridad para entrar a /authenticate
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //esto quiere decir que todas las peticiones que terminen en authenticate las permita
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated();//aca le decimos que las peticiones que no terminen en authenticate si necesitan autenticacion
+
+
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }

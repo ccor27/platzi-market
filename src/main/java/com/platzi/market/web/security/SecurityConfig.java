92cc3d67e5431e35310ed09abc182b21acae6631
cristian <cris300oso@gmail.com>
2022-01-04 17:58:10 +0100

    autorizacion con JWT

--- src/main/java/com/platzi/market/web/security/SecurityConfig.java
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.platzi.market.web.security;
 
 import com.platzi.market.domain.service.PlatziUserDetailsService;
+import com.platzi.market.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,12 +9,17 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity//para indicar que esta clase estara encargada de la seguridad
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private PlatziUserDetailsService platziUserDetailsService;
+
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,7 +31,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         //esto quiere decir que todas las peticiones que terminen en authenticate las permita
         http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
-                .anyRequest().authenticated();//aca le decimos que las peticiones que no terminen en authenticate si necesitan autenticacion
+                .anyRequest().authenticated().and().sessionManagement()//aca le decimos que las peticiones que no terminen en authenticate si necesitan autenticacion
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
 
 
     }
